35483f2e9ecb832d9948db97f170b1624f52bc15
Wes Reid <wesley.reid@gmail.com>
2022-02-01 11:58:16 -0500

    remove logging

--- packages/frontend/middleware/default.ts
+++ packages/frontend/middleware/default.ts
@@ -35,12 +35,10 @@ export default async ({
   if (store.state.auth.loggedIn) {
     const promises: Promise<any>[] = []
     if (!userStore.userId) {
-      console.log('--- 1 ---')
       const claims = decode($auth.getToken($config.authStrategy))
       await userStore.setUserId(claims[$config.authTokenIdClaim])
     }
     if (!userStore.profile) {
-      console.log('--- 2 ---')
       promises.push(userStore.fetchProfile())
     }
     await Promise.all(promises)
@@ -50,7 +48,6 @@ export default async ({
         .map((r) => app.localePath(r))
         .includes(route.path)
     ) {
-      console.log('--- 3 ---')
       redirect(app.localePath('/terms-of-use'))
     }
 
@@ -59,7 +56,6 @@ export default async ({
         .map((r) => app.localePath(r))
         .includes(route.path)
     ) {
-      console.log('--- 4 ---')
       redirect(app.localePath('/dashboard'))
     }
   }
