462b0c878ab15637d9c3aa681be02b6ba3fe85bc
Wes Reid <wesley.reid@gmail.com>
2022-02-27 08:39:42 -0500

    update toaster warning style

--- packages/frontend/layouts/dashboard.vue
+++ packages/frontend/layouts/dashboard.vue
@@ -1,6 +1,5 @@
 <template>
   <v-app>
-
     <v-overlay style="text-align: center" :z-index="10000" :value="overlay">
       <p>You are required to login again if using MyFile more than 1 hour.</p>
       <v-btn class="white--text" color="teal" @click="logout()">
@@ -10,19 +9,26 @@
     <v-snackbar
       v-model="showTimeoutWarningMessage"
       :timeout="warningMsgTimeoutMs"
-      color="red"
+      color="red darken-3"
       text
+      absolute
+      bottom
+      right
+      outlined
+      style="z-index: 10;"
     >
-      {{ timeoutWarningMessage }}
+      <div style="display: flex; flex-direction: row;">
+        <div style="flex-grow: 1; align-self: center;">{{ timeoutWarningMessage }}</div>
 
         <v-btn
           color="blue"
           text
           v-bind="attrs"
-          @click="snackbar = false"
+          @click="showTimeoutWarningMessage = false"
         >
           Close
         </v-btn>
+      </div>
     </v-snackbar>
 
     <SideNav />
@@ -40,9 +46,10 @@ import { parseJwt } from '../lib/utils'
 export default class DashboardLayout extends Vue {
   overlay = false
   authTokenKey = 'auth._token.oauth2'
-  logoutUrl = 'https://accounts-nonprd.nyc.gov/account/idpLogout.htm?x-frames-allow-from=https%3A%2F%2Fd3gtg3qw3q3xz9.cloudfront.net'
-  warningMsgTimeoutMs = 10000
-  showTimeoutWarningMessage = false
+  logoutUrl =
+    'https://accounts-nonprd.nyc.gov/account/idpLogout.htm?x-frames-allow-from=https%3A%2F%2Fd3gtg3qw3q3xz9.cloudfront.net'
+  warningMsgTimeoutMs = 10 * 1000
+  showTimeoutWarningMessage = true
   timeoutWarningMessage = ''
 
   mounted() {
@@ -53,7 +60,7 @@ export default class DashboardLayout extends Vue {
   }
   async logout() {
     // this.overlay = false
-    window.open(this.logoutUrl, '_blank');
+    window.open(this.logoutUrl, '_blank')
     window.focus()
     // @ts-ignore
     await window.cookieStore.delete(this.authTokenKey)
@@ -102,12 +109,16 @@ export default class DashboardLayout extends Vue {
         console.log(`${minutes}m ${seconds}s remaining.`)
         if (timeRemaining <= warning1AtMinute && !warning1Displayed) {
           warning1Displayed = true
-          this.timeoutWarningMessage = `You will be forced to login again in less than ${warning1AtMinute - timeoutAtMinute} minutes.`
+          this.timeoutWarningMessage = `You will be forced to login again in less than ${
+            warning1AtMinute - timeoutAtMinute
+          } minutes.`
           this.showTimeoutWarningMessage = true
           console.log(this.timeoutWarningMessage)
         } else if (timeRemaining < warning2AtMinute && !warning2Displayed) {
           warning2Displayed = true
-          this.timeoutWarningMessage = `You will be forced to login again in less than ${warning2AtMinute - timeoutAtMinute} minutes.`
+          this.timeoutWarningMessage = `You will be forced to login again in less than ${
+            warning2AtMinute - timeoutAtMinute
+          } minutes.`
           this.showTimeoutWarningMessage = true
           console.log(this.timeoutWarningMessage)
         } else if (timeRemaining <= timeoutAtMinute && !forceLoginModalOpen) {
