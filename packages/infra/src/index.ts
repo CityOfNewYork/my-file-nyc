2fab442c51a711b13ce4c6257631165182d3c70e
Wes Reid <wesley.reid@gmail.com>
2022-05-17 16:19:22 -0400

    stack updates

--- packages/infra/src/index.ts
+++ packages/infra/src/index.ts
@@ -3,12 +3,12 @@ import 'source-map-support/register'
 import * as cdk from '@aws-cdk/core'
 import { CiCdStack, Props as CiCdStackProps } from './cicd-stack'
 import fs = require('fs')
-import cdkConfig from './cdk-config';
+import cdkConfig from './cdk-config'
 
 const app = new cdk.App()
 
 // read in config - could do with some validation
-const { pipeline: stackProps } = cdkConfig;
+const { pipeline: stackProps } = cdkConfig
 new CiCdStack(app, 'CiCd', stackProps as CiCdStackProps)
 
 // optionally, add all the same stacks to the current app for direct deployment
