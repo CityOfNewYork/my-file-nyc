505228a360bdd903af2d61cf9b3261723f676891
Pavel Ilin <pashuntiy@gmail.com>
2022-09-07 12:40:13 -0400

    MYF-209: helper text on login page, MYF-210: FAQ login text, MYF-193: date input validation

--- packages/frontend/components/Settings/Settings.vue
+++ packages/frontend/components/Settings/Settings.vue
@@ -4,15 +4,16 @@
       <CityLogo class="mx-auto my-8" />
       <p class="my-8" v-html="boldMyFile"></p>
     </div>
-    <ValidationObserver ref="observer">
-      <v-form @submit.prevent>
-        <ValidationProvider rules="required|max:255">
+    <ValidationObserver ref="observer" v-slot="{ invalid }">
+      <v-form @submit.prevent ref="form">
+        <ValidationProvider
+          rules="required|max:50|min:2|alpha_spaces"
+          v-slot="{ errors }"
+        >
           <p class="subtitle-1 mt-1">
             {{ $t('account.whatIsYourFirstName') }}
           </p>
           <v-text-field
-            required
-            :rules="rules.required"
             outlined
             v-model="givenName"
             type="text"
@@ -20,10 +21,14 @@
               accountProfile.givenName ? '' : $t('account.firstName')
             "
           />
+          <span class="invalid">{{ errors[0] }}</span>
+        </ValidationProvider>
+        <ValidationProvider
+          rules="required|max:50|min:2|alpha_spaces"
+          v-slot="{ errors }"
+        >
           <p class="subtitle-1">{{ $t('account.whatIsYourLastName') }}</p>
           <v-text-field
-            required
-            :rules="rules.required"
             outlined
             v-model="familyName"
             type="text"
@@ -31,45 +36,64 @@
               accountProfile.familyName ? '' : $t('account.lastName')
             "
           />
-          <p class="subtitle-1">{{ $t('account.whatIsYourDob') }}</p>
-          <div class="d-flex justify-space-between">
-            <div>
+          <span class="invalid">{{ errors[0] }}</span>
+        </ValidationProvider>
+
+        <p class="subtitle-1">{{ $t('account.whatIsYourDob') }}</p>
+        <div class="d-flex justify-space-between">
+          <div class="inputContainer">
+            <ValidationProvider
+              rules="between:1,12|required"
+              v-slot="{ errors }"
+            >
               <label>Month</label>
               <v-text-field
+                type="number"
                 outlined
                 v-model="month"
-                :rules="rules.month"
                 :class="$vuetify.breakpoint.mdAndDown ? 'input' : ''"
               />
-            </div>
-            <div>
+              <span class="invalid">{{ errors[0] }}</span>
+            </ValidationProvider>
+          </div>
+          <div class="inputContainer">
+            <ValidationProvider
+              rules="required|between:1,31"
+              v-slot="{ errors }"
+            >
               <label>Day</label>
               <v-text-field
+                type="number"
                 outlined
                 v-model="day"
-                :rules="rules.day"
                 :class="$vuetify.breakpoint.mdAndDown ? 'input' : ''"
               />
-            </div>
-            <div>
+              <span class="invalid">{{ errors[0] }}</span>
+            </ValidationProvider>
+          </div>
+          <div class="inputContainer">
+            <ValidationProvider
+              rules="required|between:1900,2022|max:4"
+              v-slot="{ errors }"
+            >
               <label>Year</label>
               <v-text-field
+                type="number"
                 outlined
                 v-model="year"
-                :rules="rules.year"
                 :class="$vuetify.breakpoint.mdAndDown ? 'input' : ''"
               />
-            </div>
+              <span class="invalid">{{ errors[0] }}</span>
+            </ValidationProvider>
           </div>
-
+        </div>
+        <ValidationProvider rules="required|max:10|min:7" v-slot="{ errors }">
           <div>
             <p class="subtitle-1">
               {{ $t('account.whatIsYourDshCaseNumber') }}
             </p>
             <p>{{ $t('account.dhsShow') }}</p>
             <v-text-field
-              required
-              :rules="rules.caseNumber"
               outlined
               v-model="dhsCaseNumber"
               type="text"
@@ -77,25 +101,26 @@
                 accountProfile.caseNumber ? '' : $t('account.caseNumber')
               "
             />
+            <span class="invalid">{{ errors[0] }}</span>
           </div>
         </ValidationProvider>
       </v-form>
+      <v-btn
+        color="primary white--text"
+        class="px-8 d-flex justify-center"
+        style="
+          background-color: #2157e4;
+          height: 3rem;
+          align-items: center;
+          margin: auto;
+          bottom: 1rem;
+        "
+        :disabled="invalid"
+        @click="save"
+      >
+        {{ $t('controls.save') }}
+      </v-btn>
     </ValidationObserver>
-    <v-btn
-      color="primary white--text"
-      class="px-8 d-flex justify-center"
-      style="
-        background-color: #2157e4;
-        height: 3rem;
-        align-items: center;
-        margin: auto;
-        bottom: 1rem;
-      "
-      :disabled="isValid"
-      @click="save"
-    >
-      {{ $t('controls.save') }}
-    </v-btn>
   </v-container>
   <v-container class="pa-8" v-else>
     <div class="d-flex justify-space-between">
@@ -149,7 +174,39 @@
 
 <script lang="ts">
 import { Vue, Prop, Component } from 'nuxt-property-decorator'
-import { ValidationObserver, ValidationProvider } from 'vee-validate'
+import { ValidationObserver, ValidationProvider, extend } from 'vee-validate'
+import {
+  required,
+  between,
+  max,
+  alpha_spaces,
+  min,
+} from 'vee-validate/dist/rules'
+
+extend('required', {
+  ...required,
+  message: 'This field is required',
+})
+
+extend('max', {
+  ...max,
+  message: 'Input is invalid',
+})
+
+extend('min', {
+  ...min,
+  message: 'Input is invalid',
+})
+
+extend('between', {
+  ...between,
+  message: 'Input is invalid',
+})
+
+extend('alpha_spaces', {
+  ...alpha_spaces,
+  message: 'Input is invalid',
+})
 
 @Component({
   components: {
@@ -171,40 +228,40 @@ export default class Settings extends Vue {
       dob: this.dob,
       dhsCaseNumber: this.dhsCaseNumber,
       editMode: this.editMode,
-      rules: {
-        required: [
-          (v: any) => (v || '').length > 0 || 'This field is required',
-        ],
-        caseNumber: [
-          (v: any) => (v || '').length > 3 || 'Case number is required',
-        ],
-        year: [
-          (v: any) => (v || '').length > 0 || 'This field is required',
-          (v: any) => Number.isInteger(Number(v)) || 'Have to be a number',
-          (v: any) => (v || '').length <= 4 || 'Year have to have 4 digits',
-          (v: any) => (v || '').length >= 4 || 'Year have to have 4 digits',
-          (v: any) =>
-            Number.parseInt(v) <= new Date().getFullYear() ||
-            'Are you from the future?',
-          (v: any) => Number.parseInt(v) >= 1900 || 'Are you a time traveler?',
-        ],
-        day: [
-          (v: any) => Number.isInteger(Number(v)) || 'Have to be a number',
-          (v: any) => Number.parseInt(v) <= 31 || 'Have to be less then 31',
-          (v: any) => Number.parseInt(v) > 0 || 'Day have to be above 0',
-          (v: any) => (v || '').length <= 2 || 'Day have to have 2 digits',
-          (v: any) =>
-            (v || '').length >= 1 || 'Date have to have at least 1 digit',
-        ],
-        month: [
-          (v: any) => Number.isInteger(Number(v)) || 'Have to be a number',
-          (v: any) => Number.parseInt(v) <= 12 || 'There are only 12 months',
-          (v: any) => Number.parseInt(v) > 0 || 'Month have to be above 0',
-          (v: any) => (v || '').length <= 2 || 'Month have to have 2 digits',
-          (v: any) =>
-            (v || '').length >= 1 || 'Month have to have at least 1 digit',
-        ],
-      },
+      // rules: {
+      //   required: [
+      //     (v: any) => (v || '').length > 0 || 'This field is required',
+      //   ],
+      //   caseNumber: [
+      //     (v: any) => (v || '').length > 3 || 'Case number is required',
+      //   ],
+      //   year: [
+      //     (v: any) => (v || '').length > 0 || 'This field is required',
+      //     (v: any) => Number.isInteger(Number(v)) || 'Have to be a number',
+      //     (v: any) => (v || '').length <= 4 || 'Year have to have 4 digits',
+      //     (v: any) => (v || '').length >= 4 || 'Year have to have 4 digits',
+      //     (v: any) =>
+      //       Number.parseInt(v) <= new Date().getFullYear() ||
+      //       'Are you from the future?',
+      //     (v: any) => Number.parseInt(v) >= 1900 || 'Are you a time traveler?',
+      //   ],
+      //   day: [
+      //     (v: any) => Number.isInteger(Number(v)) || 'Have to be a number',
+      //     (v: any) => Number.parseInt(v) <= 31 || 'Have to be less then 31',
+      //     (v: any) => Number.parseInt(v) > 0 || 'Day have to be above 0',
+      //     (v: any) => (v || '').length <= 2 || 'Day have to have 2 digits',
+      //     (v: any) =>
+      //       (v || '').length >= 1 || 'Date have to have at least 1 digit',
+      //   ],
+      //   month: [
+      //     (v: any) => Number.isInteger(Number(v)) || 'Have to be a number',
+      //     (v: any) => Number.parseInt(v) <= 12 || 'There are only 12 months',
+      //     (v: any) => Number.parseInt(v) > 0 || 'Month have to be above 0',
+      //     (v: any) => (v || '').length <= 2 || 'Month have to have 2 digits',
+      //     (v: any) =>
+      //       (v || '').length >= 1 || 'Month have to have at least 1 digit',
+      //   ],
+      // },
     }
   }
 
@@ -253,21 +310,6 @@ export default class Settings extends Vue {
     this.editMode = !this.editMode
   }
 
-  get isValid() {
-    if (
-      !!this.givenName &&
-      !!this.familyName &&
-      !!this.day &&
-      !!this.month &&
-      !!this.year &&
-      !!this.dhsCaseNumber
-    ) {
-      return false
-    } else {
-      return true
-    }
-  }
-
   get boldMyFile() {
     if (!this.hasAccepted) {
       return this.settingsFirstRun.replace('#', '<b>').replace('&', '</b>')
@@ -280,19 +322,23 @@ export default class Settings extends Vue {
   }
 
   async save() {
-    this.dob = this.dobConstruct()
+    if ((this.$refs.form as Vue & { validate: () => boolean }).validate()) {
+      this.dob = this.dobConstruct()
 
-    const data = {
-      givenName: this.givenName,
-      familyName: this.familyName,
-      dob: this.dob,
-      dhsCaseNumber: this.dhsCaseNumber,
-    }
+      const data = {
+        givenName: this.givenName,
+        familyName: this.familyName,
+        dob: this.dob,
+        dhsCaseNumber: this.dhsCaseNumber,
+      }
 
-    if (this.location == '/account') {
-      this.patchUser(data)
+      if (this.location == '/account') {
+        this.patchUser(data)
+      } else {
+        this.submit(data)
+      }
     } else {
-      this.submit(data)
+      console.log('Input is invalid')
     }
   }
 
@@ -305,5 +351,14 @@ export default class Settings extends Vue {
 <style scoped lang="scss">
 .input {
   width: 80%;
+  height: 4rem;
+}
+
+.inputContainer {
+  margin-bottom: 10px;
+}
+
+.invalid {
+  color: red;
 }
 </style>
