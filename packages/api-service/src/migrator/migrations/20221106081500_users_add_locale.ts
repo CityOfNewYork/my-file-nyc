d70ecb0731260507b3acbf335487374bded3d027
Wes Reid <wesley.reid@gmail.com>
2022-11-06 14:03:22 +0100

    resolve issue with migrator

---
+++ packages/api-service/src/migrator/migrations/20221106081500_users_add_locale.ts
@@ -0,0 +1,13 @@
+import * as Knex from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.alterTable('users', (t) => {
+    t.string('locale', 255).nullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.alterTable('users', (t) => {
+    t.dropColumns('locale')
+  })
+}
