758988b82a8119e6768794670a2c2552177769c2
Pavel Ilin <pashuntiy@gmail.com>
2023-07-07 16:29:48 -0400

    language selector component added

--- packages/frontend/components/Settings/Settings.vue
+++ packages/frontend/components/Settings/Settings.vue
@@ -1,192 +1,205 @@
 <template>
-  <v-container v-if="editMode" class="pa-5 mb-10">
-    <div class="mt-1" style="text-align: center">
-      <CityLogo class="mx-auto my-8" />
-      <p class="my-8" v-html="boldMyFile"></p>
-    </div>
-    <ValidationObserver ref="observer" v-slot="{ invalid }">
-      <v-form @submit.prevent ref="form">
-        <ValidationProvider
-          name="First Name"
-          rules="required|max:50|min:2|alpha_spaces"
-          v-slot="{ errors }"
-        >
-          <p class="subtitle-1 mt-1">
-            {{ $t('account.whatIsYourFirstName') }}
-          </p>
-          <v-text-field
-            outlined
-            v-model="givenName"
-            type="text"
-            :placeholder="
-              accountProfile.givenName ? '' : $t('account.firstName')
-            "
-          />
-          <span class="invalid">{{ errors[0] }}</span>
-        </ValidationProvider>
-        <ValidationProvider
-          name="Last Name"
-          rules="required|max:50|min:2|alpha_spaces"
-          v-slot="{ errors }"
-        >
-          <p class="subtitle-1">{{ $t('account.whatIsYourLastName') }}</p>
-          <v-text-field
-            outlined
-            v-model="familyName"
-            type="text"
-            :placeholder="
-              accountProfile.familyName ? '' : $t('account.lastName')
-            "
-          />
-          <span class="invalid">{{ errors[0] }}</span>
-        </ValidationProvider>
-
-        <p class="subtitle-1">{{ $t('account.whatIsYourDob') }}</p>
-        <div class="d-flex justify-space-between">
-          <div class="inputContainer">
-            <ValidationProvider
-              name="Month"
-              rules="between:1,12|required"
-              v-slot="{ errors }"
-            >
-              <label>Month</label>
-              <v-text-field
-                type="number"
-                outlined
-                v-model="month"
-                :class="$vuetify.breakpoint.mdAndDown ? 'input' : ''"
-              />
-              <span class="invalid">{{ errors[0] }}</span>
-            </ValidationProvider>
-          </div>
-          <div class="inputContainer">
-            <ValidationProvider
-              name="Day"
-              rules="required|between:1,31"
-              v-slot="{ errors }"
-            >
-              <label>Day</label>
-              <v-text-field
-                type="number"
-                outlined
-                v-model="day"
-                :class="$vuetify.breakpoint.mdAndDown ? 'input' : ''"
-              />
-              <span class="invalid">{{ errors[0] }}</span>
-            </ValidationProvider>
-          </div>
-          <div class="inputContainer">
-            <ValidationProvider
-              name="Year"
-              rules="required|between:1900,2022|max:4"
-              v-slot="{ errors }"
-            >
-              <label>Year</label>
-              <v-text-field
-                type="number"
-                outlined
-                v-model="year"
-                :class="$vuetify.breakpoint.mdAndDown ? 'input' : ''"
-              />
-              <span class="invalid">{{ errors[0] }}</span>
-            </ValidationProvider>
-          </div>
-        </div>
-        <ValidationProvider
-          name="Case Number"
-          rules="required|max:20|min:4"
-          v-slot="{ errors }"
-        >
-          <div>
-            <p class="subtitle-1">
-              {{ $t('account.whatIsYourDshCaseNumber') }}
+  <v-container v-if="loading">
+    <v-container v-if="editMode" class="pa-5 mb-10">
+      <div class="mt-1" style="text-align: center">
+        <CityLogo class="mx-auto my-8" />
+        <p class="my-8" v-html="boldMyFile"></p>
+      </div>
+      <ValidationObserver ref="observer" v-slot="{ invalid }">
+        <v-form @submit.prevent ref="form">
+          <ValidationProvider
+            name="First Name"
+            rules="required|max:50|min:2|alpha_spaces"
+            v-slot="{ errors }"
+          >
+            <p class="subtitle-1 mt-1">
+              {{ $t('account.whatIsYourFirstName') }}
             </p>
-            <p>{{ $t('account.dhsShow') }}</p>
             <v-text-field
               outlined
-              v-model="dhsCaseNumber"
+              v-model="givenName"
               type="text"
               :placeholder="
-                accountProfile.caseNumber ? '' : $t('account.caseNumber')
+                accountProfile.givenName ? '' : $t('account.firstName')
               "
             />
             <span class="invalid">{{ errors[0] }}</span>
+          </ValidationProvider>
+          <ValidationProvider
+            name="Last Name"
+            rules="required|max:50|min:2|alpha_spaces"
+            v-slot="{ errors }"
+          >
+            <p class="subtitle-1">{{ $t('account.whatIsYourLastName') }}</p>
+            <v-text-field
+              outlined
+              v-model="familyName"
+              type="text"
+              :placeholder="
+                accountProfile.familyName ? '' : $t('account.lastName')
+              "
+            />
+            <span class="invalid">{{ errors[0] }}</span>
+          </ValidationProvider>
+
+          <p class="subtitle-1">{{ $t('account.whatIsYourDob') }}</p>
+          <div class="d-flex justify-space-between">
+            <div class="inputContainer">
+              <ValidationProvider
+                name="Month"
+                rules="between:1,12|required"
+                v-slot="{ errors }"
+              >
+                <label>Month</label>
+                <v-text-field
+                  type="number"
+                  outlined
+                  v-model="month"
+                  :class="$vuetify.breakpoint.mdAndDown ? 'input' : ''"
+                />
+                <span class="invalid">{{ errors[0] }}</span>
+              </ValidationProvider>
+            </div>
+            <div class="inputContainer">
+              <ValidationProvider
+                name="Day"
+                rules="required|between:1,31"
+                v-slot="{ errors }"
+              >
+                <label>Day</label>
+                <v-text-field
+                  type="number"
+                  outlined
+                  v-model="day"
+                  :class="$vuetify.breakpoint.mdAndDown ? 'input' : ''"
+                />
+                <span class="invalid">{{ errors[0] }}</span>
+              </ValidationProvider>
+            </div>
+            <div class="inputContainer">
+              <ValidationProvider
+                name="Year"
+                rules="required|between:1900,2022|max:4"
+                v-slot="{ errors }"
+              >
+                <label>Year</label>
+                <v-text-field
+                  type="number"
+                  outlined
+                  v-model="year"
+                  :class="$vuetify.breakpoint.mdAndDown ? 'input' : ''"
+                />
+                <span class="invalid">{{ errors[0] }}</span>
+              </ValidationProvider>
+            </div>
           </div>
-        </ValidationProvider>
-        <!-- language changer component -->
-        <LanguageChanger textColor="black" outlined="true" />
-      </v-form>
+          <ValidationProvider
+            name="Case Number"
+            rules="required|max:20|min:4"
+            v-slot="{ errors }"
+          >
+            <div>
+              <p class="subtitle-1">
+                {{ $t('account.whatIsYourDshCaseNumber') }}
+              </p>
+              <p>{{ $t('account.dhsShow') }}</p>
+              <v-text-field
+                outlined
+                v-model="dhsCaseNumber"
+                type="text"
+                :placeholder="
+                  accountProfile.caseNumber ? '' : $t('account.caseNumber')
+                "
+              />
+              <span class="invalid">{{ errors[0] }}</span>
+            </div>
+          </ValidationProvider>
+          <!-- language changer component -->
+          <LanguageSelector :locale="locale" :localeChange="localeChange" textColor="black" outlined="true" />
+        </v-form>
+        <v-btn
+          color="primary white--text"
+          class="px-8 d-flex justify-center"
+          style="
+            background-color: #2157e4;
+            height: 3rem;
+            align-items: center;
+            margin: auto;
+            bottom: 1rem;
+          "
+          :disabled="invalid"
+          @click="save"
+        >
+          {{ $t('controls.save') }}
+        </v-btn>
+      </ValidationObserver>
+    </v-container>
+    <v-container class="pa-8" v-else>
+      <div class="d-flex justify-space-between">
+        <div class="pt-2 v-toolbar__title">
+          {{ `${$t('navigation.settings')}` }}
+        </div>
+      </div>
+      <div
+        :class="
+          $vuetify.breakpoint.smAndUp ? 'mt-10  justify-space-between' : 'mt-10'
+        "
+      >
+        <div>
+          <p class="subtitle-1">{{ $t('account.firstName') }}</p>
+          <p :key="givenName">
+            {{ givenName }}
+          </p>
+        </div>
+        <div>
+          <p class="subtitle-1">{{ $t('account.lastName') }}</p>
+          <p :key="familyName">
+            {{ familyName }}
+          </p>
+        </div>
+        <div>
+          <p class="subtitle-1">{{ $t('account.dob') }}</p>
+          <p :key="dob">{{ dob }}</p>
+        </div>
+        <div>
+          <p class="subtitle-1">{{ $t('account.caseNumber') }}</p>
+          <p :key="dhsCaseNumber">
+            {{ dhsCaseNumber }}
+          </p>
+        </div>
+        <div>
+          <p class="subtitle-1">{{ $t('account.locale') }}</p>
+          <p :key="locale">
+            {{ localToRender }}
+          </p>
+        </div>
+      </div>
       <v-btn
         color="primary white--text"
-        class="px-8 d-flex justify-center"
+        class="px-8 d-flex justify-center -10"
         style="
           background-color: #2157e4;
           height: 3rem;
           align-items: center;
           margin: auto;
-          bottom: 1rem;
         "
-        :disabled="invalid"
-        @click="save"
+        @click="edit"
       >
-        {{ $t('controls.save') }}
+        {{ $t('controls.edit') }}
       </v-btn>
-    </ValidationObserver>
+    </v-container>
   </v-container>
-  <v-container class="pa-8" v-else>
-    <div class="d-flex justify-space-between">
-      <div class="pt-2 v-toolbar__title">
-        {{ `${$t('navigation.settings')}` }}
-      </div>
-    </div>
-    <div
-      :class="
-        $vuetify.breakpoint.smAndUp ? 'mt-10  justify-space-between' : 'mt-10'
-      "
+  <div v-else>
+    <v-card
+      v-for="i in new Array(5)"
+      :key="i"
+      class="mx-auto mb-4"
+      max-width="700"
+      outlined
     >
-      <div>
-        <p class="subtitle-1">{{ $t('account.firstName') }}</p>
-        <p :key="givenName">
-          {{ givenName }}
-        </p>
-      </div>
-      <div>
-        <p class="subtitle-1">{{ $t('account.lastName') }}</p>
-        <p :key="familyName">
-          {{ familyName }}
-        </p>
-      </div>
-      <div>
-        <p class="subtitle-1">{{ $t('account.dob') }}</p>
-        <p :key="dob">{{ dob }}</p>
-      </div>
-      <div>
-        <p class="subtitle-1">{{ $t('account.caseNumber') }}</p>
-        <p :key="dhsCaseNumber">
-          {{ dhsCaseNumber }}
-        </p>
-      </div>
-      <div>
-        <p class="subtitle-1">{{ $t('account.locale') }}</p>
-        <p :key="locale">
-          {{ localToRender }}
-        </p>
-      </div>
-    </div>
-    <v-btn
-      color="primary white--text"
-      class="px-8 d-flex justify-center -10"
-      style="
-        background-color: #2157e4;
-        height: 3rem;
-        align-items: center;
-        margin: auto;
-      "
-      @click="edit"
-    >
-      {{ $t('controls.edit') }}
-    </v-btn>
-  </v-container>
+      <v-skeleton-loader type="list-item-two-line"></v-skeleton-loader>
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts">
@@ -224,6 +237,7 @@ export default class Settings extends Vue {
 
   data() {
     return {
+      loading: true,
       givenName: this.givenName,
       familyName: this.familyName,
       dob: this.dob,
@@ -258,6 +272,7 @@ export default class Settings extends Vue {
   locale = ''
   localToRender = ''
   languagesObject = ''
+  loading = true
 
   settingsFirstRun = this.$t('navigation.settingsFirstRun') as string
 
@@ -274,8 +289,11 @@ export default class Settings extends Vue {
   }
 
   updated() {
-    this.locale = this.accountProfile.locale
     this.localToRender = this.languagesObject[this.locale as any];
+  }
+
+  localeChange(locale: string) {
+    this.locale = locale
   }
 
   dobDistruct(dob: string) {
@@ -292,12 +310,16 @@ export default class Settings extends Vue {
   }
 
   async patchUser(data: object) {
+    this.loading = false
     const response = await this.$store.dispatch('user/patchProfile', data)
+
     this.givenName = response.givenName
     this.familyName = response.familyName
     this.dob = response.dob
     this.dhsCaseNumber = response.dhsCaseNumber
     this.locale = response.locale
+    this.$i18n.locale = response.locale
+    this.loading = true
   }
 
   get boldMyFile() {
@@ -320,10 +342,10 @@ export default class Settings extends Vue {
         familyName: this.familyName,
         dob: this.dob,
         dhsCaseNumber: this.dhsCaseNumber,
-        locale: this.$i18n.locale,
+        locale: this.locale,
       }
 
-      if (this.location == '/account') {
+      if (this.location == `/${this.$i18n.locale}/account`) {
         this.patchUser(data)
       } else {
         this.submit(data)
